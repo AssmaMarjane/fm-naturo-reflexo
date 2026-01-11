ed0828642bcec471cdecf2888c968443745b894f
AssmaMarjane <assmam@hotmail.fr>
2026-01-11 11:59:15 +0100

    retrait leaff

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,6 @@
 //Imports the necessary styles and components for the Home page.
 import "../../style/main.scss";
 import "./Intro.scss";
-import img1 from "../../assets/images/plante1.5.png";
-import img3 from "../../assets/images/fleur2.png";
-import img4 from "../../assets/images/fleur1.png";
-import img5 from "../../assets/images/plante1.5.png";
 
 function Intro() {
   return (
@@ -12,13 +8,22 @@ function Intro() {
       <div className="decor-section">
         <p className="decor-title">Découvrez mes méthodes naturelles pour retrouver vitalité et harmonie.</p>
 
-        <img src={img1} alt="leaf" className="leaf leaf1" />
-        <img src={img3} alt="leaf" className="leaf leaf2" />
-        <img src={img4} alt="leaf" className="leaf leaf4" />
-        <img src={img5} alt="leaf" className="leaf leaf5" />
       </div>
     </div>
   );
 }
 
 export default Intro;
+
+/*
+import img1 from "../../assets/images/plante1.5.png";
+import img3 from "../../assets/images/fleur2.png";
+import img4 from "../../assets/images/fleur1.png";
+import img5 from "../../assets/images/plante1.5.png";
+
+
+        <img src={img1} alt="leaf" className="leaf leaf1" />
+        <img src={img3} alt="leaf" className="leaf leaf2" />
+        <img src={img4} alt="leaf" className="leaf leaf4" />
+        <img src={img5} alt="leaf" className="leaf leaf5" />
+*/
